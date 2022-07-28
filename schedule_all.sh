38f0a7fbc52ffdb18e5843feb6596ec0fc6b8962
Dio76453 <diogo.pires.silva@ua.pt>
2022-07-28 17:04:43 +0100

    fix

--- schedule_all.sh
+++ schedule_all.sh
@@ -3,6 +3,6 @@
 # Update Satellite Information
 
 wget -qr https://www.celestrak.com/NORAD/elements/weather.txt -O /home/pi/.predict/predict.txt
-grep "NOAA 15" /home/pi/weather/predict/weather.txt -A 2 > /home/pi/.predict/predict.tle
-grep "NOAA 18" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/.predict/predict.tle
-grep "NOAA 19" /home/pi/weather/predict/weather.txt -A 2 >> /home/pi/.predict/predict.tle
+grep "NOAA 15" /home/pi/.predict/predict.txt -A 2 > /home/pi/.predict/predict.tle
+grep "NOAA 18" /home/pi/.predict/predict.txt -A 2 >> /home/pi/.predict/predict.tle
+grep "NOAA 19" /home/pi/.predict/predict.txt -A 2 >> /home/pi/.predict/predict.tle
---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+
+sudo apt install wget libncurses-dev libasound2-dev -y
+wget https://www.qsl.net/kd2bd/predict-2.3.0.tar.gz
